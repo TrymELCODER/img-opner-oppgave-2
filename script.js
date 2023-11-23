cd8b1edf57b248d18dff7d699cf907789f379b06
TrymCoder <trymel.koder@gmail.com>
2023-11-23 09:27:48 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const enlargeButton = document.getElementById('enlargeButton');
+  const resetButton = document.getElementById('resetButton');
+  const images = document.querySelectorAll('.image-container img');
+
+  enlargeButton.addEventListener('click', function () {
+      images.forEach(img => {
+          img.classList.add('enlarged');
+      });
+  });
+
+  resetButton.addEventListener('click', function () {
+      images.forEach(img => {
+          img.classList.remove('enlarged');
+      });
+  });
+});
